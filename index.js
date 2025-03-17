74ce324d46b6b0ec0efb867c86432252119c46c3
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-17 11:41:21 -0400

    "code updated"

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length
 
-document.querySelector("button").addEventListener("click",handleClick);
+
+for(var i =0; i<numberOfDrumButtons; i++){
+    document.querySelector(".drum")[i].addEventListener("click",function (){
+        alert('I got clicked!!');
+
+});
+
+}
+
+//document.querySelector("button").addEventListener("click",handleClick);
 
 /* we want our button to trigger this function when it's clicked*/
-function handleClick(){
-    alert('I got clicked!!');
-}
+//function handleClick(){
+//    alert('I got clicked!!');
+//}
+
+//other way to do it:
+//document.querySelector("button").addEventListener("click",function (){
+//    alert('I got clicked!!');
+//});
