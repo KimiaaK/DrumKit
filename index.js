7cf466a2314bf1718fb6fd62815de731ae7840d7
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-17 15:19:21 -0400

    audio was added

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 
 for(var i =0; i<numberOfDrumButtons; i++){
+
     document.querySelector(".drum")[i].addEventListener("click",function (){
-        alert('I got clicked!!');
+        
+        var audio = new Audio('./sounds/tom-1.mp3');
+        audio.play();
 
 });
 
