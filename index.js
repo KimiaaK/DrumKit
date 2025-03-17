b8df95a5a6fa09eae3f707bf2db55a413f595efb
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-17 15:39:33 -0400

    "voices added"

--- index.js
+++ index.js
@@ -4,13 +4,56 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(var i =0; i<numberOfDrumButtons; i++){
 
     document.querySelector(".drum")[i].addEventListener("click",function (){
+        var buttonInnerHTML = this.buttonInnerHTML
+    switch(buttonInnerHTML){
+        case "w":
+            var audio_tom1 = new Audio('./sounds/tom-1.mp3');
+            audio_tom1.play();
+            break;
         
-        var audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
+        case "a":
+            var audio_tom2 = new Audio('./sounds/tom-2.mp3');
+            audio_tom2.play();
+            break;
+
+        case "s":
+            var audio_tom3 = new Audio('./sounds/tom-3.mp3');
+            audio_tom3.play();
+            break;  
+            
+        case "d":
+            var audio_tom4 = new Audio('./sounds/tom-4.mp3');
+            audio_tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio('./sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "k":    
+            var crash = new Audio('./sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "l":
+            var kick = new Audio('./sounds/kick.mp3');
+            kick.play();
+            break;
+
+
+        default:
+            console.log(buttonInnerHTML)
+    }
+        
 
 });
 
 }
+
+
+
+
 
 //document.querySelector("button").addEventListener("click",handleClick);
 
