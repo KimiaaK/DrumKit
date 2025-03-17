df156c69ac2fdfb8753bf87106470029af1ae22c
Kimia Ketabforoosh <keam96@ymail.com>
2025-03-17 11:20:45 -0400

    "code updated"

---
+++ index.js
@@ -0,0 +1,7 @@
+
+document.querySelector("button").addEventListener("click",handleClick)
+
+/* we want our button to trigger this function when it's clicked*/
+function handleClick(){
+    alert('I got clicked!!');
+}
